42c59fae22f0c15ad9506a7b55df88709a05cf4e
Patrícia Martins Rocca Crestani <patricia.crestani@viceri.com.br>
2025-02-05 16:00:46 -0300

    fix: atualizando native federation

--- federation.config.js
+++ federation.config.js
@@ -6,10 +6,34 @@ module.exports = withNativeFederation({
 
   exposes: {
     './Component': './src/app.component.ts',
+    "./routes": "./src/app.routes.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto', transient: true }),
+    "@primeng/themes/types": {
+      transient: true,
+      packageInfo: {
+        entryPoint: "node_modules/@primeng/themes/index.mjs",
+        version: "19.0.5",
+        esm: true,
+      },
+    },
+    "@primeng/themes/lara": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+      transient: true,
+      includeSecondaries: false,
+      build: "separate",
+    },
+    '@primeng/themes/lara/accordion': {
+      packageInfo: {
+        entryPoint: 'node_modules/@primeng/themes/lara/accordion/index.mjs',
+        version: '19.0.5',
+        esm: true
+      }
+    },
   },
 
   skip: [
